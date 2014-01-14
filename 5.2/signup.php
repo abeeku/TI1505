d68c9f29a97899e8e380f3de2a55e91046c5e44c
Gerlof Fokkema <gerlof.fokkema@gmail.com>
2014-01-14 01:33:49 +0100

    opdracht 5.2: w3c fixes

--- 5.2/signup.php
+++ 5.2/signup.php
@@ -7,8 +7,8 @@ printheader();
 	<fieldset>
 		<legend>New User Signup:</legend>
 		<strong>Name:</strong>			<input type="text" name="name" /><br/>
-		<strong>Gender:</strong>		<input type="radio" name="gender" value="M">Male</input>
-							<input type="radio" name="gender" value="F">Female</input><br/>
+		<strong>Gender:</strong>		<input type="radio" name="gender" value="M">Male
+							<input type="radio" name="gender" value="F">Female<br/>
 		<strong>Age:</strong>			<input type="text" name="age" /><br/>
 		<strong>Personality type:</strong>	<input type="text" name="ptype" /><br/>
 		<strong>Favorite OS:</strong>		<select name="favos">
@@ -17,8 +17,8 @@ printheader();
 								<option value="Linux">Linux</option>
 								<option value="Other">other</option>
 							</select><br/>
-		<strong>Seeking:</strong>		<input type="checkbox" name="seekingm">Male</input>
-							<input type="checkbox" name="seekingf">Female</input><br/>
+		<strong>Seeking:</strong>		<input type="checkbox" name="seekingm">Male
+							<input type="checkbox" name="seekingf">Female<br/>
 		<strong>Between ages:</strong>		<input type="text" size="5" name="minage" />and<input type="text" size="5" name="maxage" /><br/>
 		<input type="submit" value="Sign Up" />
 	</fieldset>
