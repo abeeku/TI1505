e27bbf6871b63a831b0b753b65df56b6ed321fca
Gerlof Fokkema <gerlof.fokkema@gmail.com>
2014-01-16 14:03:40 +0100

    added image support

--- 5.2/results.php
+++ 5.2/results.php
@@ -15,23 +15,30 @@ foreach ($singles as $singleimp) {
 		if ($post['favos'] == $single[4])
 			$rating += 2;
 		$rating += similar_text(strtoupper($post['ptype']), $single[3]);
-		?>
-		<div class="match">
-			<p><?= $single[0] ?>
-				<img src="images/default-user.jpg" alt="image" />
-				<ul>
-					<li><strong>gender:</strong><?= $single[1] ?></li>
-					<li><strong>age:</strong><?= $single[2] ?></li>
-					<li><strong>type:</strong><?= $single[3] ?></li>
-					<li><strong>OS:</strong><?= $single[4] ?></li>
-					<li><strong>rating:</strong><?= $rating ?></li>
-				</ul>
-			</p>
-		</div>
-		<?php
-			// $single[5] == seeking genders
-			// $single[6] == min age
-			// $single[7] == max age
+
+		if ($rating >= 3) {
+
+			$file = "images/" . strtolower(str_replace(' ', '_', $single[0]) . ".jpg");
+			if (!file_exists($file))
+				$file = "images/default-user.jpg";
+			?>
+			<div class="match">
+				<p><?= $single[0] ?>
+					<img src="<?= $file ?>" alt="image" />
+					<ul>
+						<li><strong>gender:</strong><?= $single[1] ?></li>
+						<li><strong>age:</strong><?= $single[2] ?></li>
+						<li><strong>type:</strong><?= $single[3] ?></li>
+						<li><strong>OS:</strong><?= $single[4] ?></li>
+						<li><strong>rating:</strong><?= $rating ?></li>
+					</ul>
+				</p>
+			</div>
+			<?php
+				// $single[5] == seeking genders
+				// $single[6] == min age
+				// $single[7] == max age
+		}
 	}
 }
 
--- 5.2/signup.php
+++ 5.2/signup.php
@@ -6,11 +6,12 @@ printheader();
 <form action="results.php" method="get">
 	<fieldset>
 		<legend>New User Signup:</legend>
-		<strong>Name:</strong>			<input type="text" name="name" /><br/>
+		<strong>Name:</strong>			<input type="text" name="name" size="16" /><br/>
 		<strong>Gender:</strong>		<input type="radio" name="gender" value="M" />Male
 							<input type="radio" name="gender" value="F" />Female<br/>
-		<strong>Age:</strong>			<input type="text" name="age" /><br/>
-		<strong>Personality type:</strong>	<input type="text" name="ptype" /><br/>
+		<strong>Age:</strong>			<input type="text" name="age" size="5" maxlength="2" /><br/>
+		<strong>Personality type:</strong>	<input type="text" name="ptype" size="5" maxlength="4" />
+							(<a href="http://www.humanmetrics.com/cgi-win/JTypes2.asp">Don't know your type?</a>)<br/>
 		<strong>Favorite OS:</strong>		<select name="favos">
 								<option value="Windows">Windows</option>
 								<option value="Mac OS X">Mac OS X</option>
@@ -19,7 +20,8 @@ printheader();
 							</select><br/>
 		<strong>Seeking:</strong>		<input type="checkbox" name="seekingm" />Male
 							<input type="checkbox" name="seekingf" />Female<br/>
-		<strong>Between ages:</strong>		<input type="text" size="5" name="minage" />and<input type="text" size="5" name="maxage" /><br/>
+		<strong>Between ages:</strong>		<input type="text" name="minage" size="5" maxlength="2" />and
+							<input type="text" name="maxage" size="5" maxlength="2" /><br/>
 		<input type="submit" value="Sign Up" />
 	</fieldset>
 </form>
