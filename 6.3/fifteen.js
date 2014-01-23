5cb344ee5401cbfe9c92757a70c28b1619861392
Gerlof Fokkema <gerlof.fokkema@gmail.com>
2014-01-23 14:45:44 +0100

    opdracht 6.3: helemaal af

--- 6.3/fifteen.js
+++ 6.3/fifteen.js
@@ -13,45 +13,71 @@ function initialize() {
 	pieces = $$("#puzzlearea > div");
 	
 	for(var i=0; i<pieces.length; i++) {
-		// ### For each puzzle piece make sure that
-		// ### - it is at the correct position
-		// ### - the background is correct
+		pieces[i].addClassName("puzzlepiece");
+		var left = (i % 4) * 100;
+		var top = Math.floor(i / 4) * 100;
+		pieces[i].style.left = left + "px";
+		pieces[i].style.top = top + "px";
+		pieces[i].style.backgroundPosition = -left + "px " + -top + "px";
 	}
 }
 
 function setMovablePieces() {
 	for(var i=0; i<pieces.length; i++) {	
-		var curX = undefined; // ### Get the x of the current piece
-		var curY = undefined; // ### Get the y of the current piece
+		var curX = parseInt(pieces[i].style.left); // ### Get the x of the current piece
+		var curY = parseInt(pieces[i].style.top); // ### Get the y of the current piece
 		
 		if(isMovable(curX,curY)) {
 			// ### piece has the style of a movable piece
 			// ### piece can be clicked to move it
+			pieces[i].addClassName("movablepiece");
+			pieces[i].observe("click", moveThis);
 		} else {
 			// ### piece no longer has the style of a movable piece
 			// ### piece can no longer be clicked to move it
+			pieces[i].removeClassName("movablepiece");
+			pieces[i].stopObserving("click", moveThis);
 		}
 	}
 }
 
 function isMovable(x,y) {
 	// ### returns true if te piece at (x,y) is next to the empty square at (emptyX,emptyY)
-	return undefined;
+	var isMovable = false;
+	if ((Math.abs(emptyX - x) + Math.abs(emptyY - y)) == 100) {
+		isMovable = true;
+	}
+	return isMovable;
 }
 
 function moveThis() {
-	move(this);
+	move(this, false);
 }
 
-function move(piece) {
-	var curX = undefined; // ### Get the x of the current piece (almost the same as in setMovablePieces)
-	var curY = undefined; // ### Get the y of the current piece (almost the same as in setMovablePieces)
+function move(piece, shuffle) {
+	var curX = parseInt(piece.style.left); // ### Get the x of the current piece (almost the same as in setMovablePieces)
+	var curY = parseInt(piece.style.top); // ### Get the y of the current piece (almost the same as in setMovablePieces)
+	
+	if (shuffle) {
+		piece.style.left = emptyX + "px";
+		piece.style.top = emptyY + "px";
+	} else {
+		new Effect.Move(piece, { x: emptyX, y: emptyY, mode: 'absolute', afterFinish: setMovablePieces });
+	}
 	
 	// ### move the piece to the empty location
+	emptyX = curX;
+	emptyY = curY;
 	
 	setMovablePieces(); 
 }
 
 function shuffle() {
 	// ### Implementation of the shuffle algorithm
+	for (var i = 0; i < 1000; i++) {
+		var movable = $$(".movablepiece");
+		var piece = movable[Math.floor(Math.random() * movable.length)];
+		move(piece, true);
+	}
 }
+
